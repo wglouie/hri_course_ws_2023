e3953d3b4e5df849da070f0cb7596231f8d68690
Geoffrey Louie <louie@oakland.edu>
2023-03-04 13:40:09 -0500

    Week 10-11: Chatbots

---
+++ src/python_examples/factorial_main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from Factorial import *
+
+if __name__ == '__main__':
+    f = Factorial(5)
+    g = Factorial()
+
+    fact_val1 = f.compute()
+    fact_val2 = g.compute()
+
+    print "Factorial value 1: {}".format(fact_val1)
+    print "Factorial value 2: {}".format(fact_val2)
+
+print screen baby
